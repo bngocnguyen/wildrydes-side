1d32ebeb76a75de34dfde74c0f58e3d5daa9f8a3
Ngoc Nguyen <bngocnguyen94@gmail.com>
2025-10-26 10:58:07 +0700

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_4DXCfuYmV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3qp46a5n73tt78m1sr1eu28drg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
